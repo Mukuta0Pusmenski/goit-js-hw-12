f1e5673b918632d36a7a374f9fd28d2622c298b9
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2024-12-02 10:14:40 +0200

    test2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css'; // правильний шлях
 import { fetchImages } from './js/pixabay-api.js';
 import { displayImages } from './js/render-functions.js';
 
