8cd4fc073ed575400cc32479ba6f4ec2d6e1230f
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2024-12-05 15:48:33 +0200

    test10

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import iziToast from 'izitoast';
 import { fetchImages } from './js/pixabay-api.js';
 import { displayImages } from './js/render-functions.js';
 
